762d79b0ed329fc2a39d954f37802bee88a3dc2c
machadolhes <souza.machado.rodrigo@gmail.com>
2018-06-30 01:11:37 -0300

    Remove package git and add packages screenfetch and sublime-text

--- lea.sh
+++ lea.sh
@@ -8,6 +8,13 @@ sudo apt dist-upgrade -y
 #install stuff
 sudo apt install build-essential transmission git gimp plank vim vlc -y
 
+#install sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
+
 #install papirus icon theme
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt update 
