13a6100c28f2247298f18efc62762e9b1d2f4c12
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2019-10-23 22:09:33 -0300

    Update lea.sh

--- lea.sh
+++ lea.sh
@@ -6,16 +6,4 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 
 #install stuff
-sudo apt install build-essential transmission gedit gimp plank vim vlc ubuntu-restricted-extras screenfetch htop preload -y
-
-#install sublime-text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install apt-transport-https -y
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text -y
-
-#install papirus icon theme
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt update 
-sudo apt install papirus-icon-theme -y
+sudo apt install build-essential transmission gedit gimp plank vim vlc papirus-icon-theme ubuntu-restricted-extras screenfetch htop sublime-text preload -y
